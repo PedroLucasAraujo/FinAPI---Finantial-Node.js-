a79da2c8aa187057a563308c46fb93642372a939
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-19 21:24:05 -0300

    feat: criando requisições http com os cinco metódos

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ignite", (request, response) => {
-  return response.json({
-    message: "Hello World Ignite - Fundalmentals of NodeJs",
-  });
+/**
+ *  GET - Buscar uma informação dentro do servidor
+ *  POST - Inserir uma Informação no servidor
+ *  PUT - Alterar uma informação no servidor
+ *  PATCH - Alterar uma informação específica no servidor
+ *  DELETE - Deletar uma informação no servidor
+ */
+
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333);
