6ab15cc570ffeffa16d4306f20fbea15796ccb99
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-19 21:17:08 -0300

    feat: adicionando nodemon no projeto + criando script de dev

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/ignite", (request, response) => {
-  return response.json({ message: "Hello World Ignite!" });
+  return response.json({
+    message: "Hello World Ignite - Fundalmentals of NodeJs",
+  });
 });
 
 app.listen(3333);
