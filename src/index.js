5ec40308d91e40bf1a0d3098fe0427b34368bd2a
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-23 17:47:38 -0300

    docs: project requisits and business rules

--- src/index.js
+++ src/index.js
@@ -2,45 +2,4 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
-
-/**
- *  GET - Buscar uma informação dentro do servidor
- *  POST - Inserir uma Informação no servidor
- *  PUT - Alterar uma informação no servidor
- *  PATCH - Alterar uma informação específica no servidor
- *  DELETE - Deletar uma informação no servidor
- */
-
-/**
- * Tipos de Parâmetros
- *
- * Route Params => Identificar um recurso editar/deletar/buscar
- * Query Params => Paginação / Filtro (url/courses?page=1&order=asc)
- * Body Params => Os objetos para Inserção/alteração (JSON)
- */
-
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
-
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-  const params = request.params;
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
-});
-
 app.listen(3333);
